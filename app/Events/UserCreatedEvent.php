bf42bd1cceee4f286261e3b56f39a172e278169c
addabenkoceir13 <addamohamed67@gmail.com>
2025-10-13 23:39:40 +0100

    feat: create new file and make update

--- app/Events/UserCreatedEvent.php
+++ app/Events/UserCreatedEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,7 +15,7 @@ class UserCreatedEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $user
+    public $user;
 
     /**
      * Create a new event instance.
